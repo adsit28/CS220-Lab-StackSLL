66dc8c8693211a6e86e2e2dc3f433fad743174db
Adrian de Freitas <Adefreit@users.noreply.github.com>
2023-02-08 22:23:54 -0700

    Removed stackPush

--- stackAsLinkedList.c
+++ stackAsLinkedList.c
@@ -8,13 +8,3 @@
  */
 #include "stackAsLinkedList.h"
 
-/** stackPush()
- * @brief - puts an element onto the top of the stack
- * @param stack - a ptr to the stack structure
- * @param element - the character to push onto the stack
- * @pre   - stack before element added
- * @post  - stack one element taller
- */
-void stackPush(StackAsLinkedList* stack, double element) {
-    appendElementLinkedList(stack, element);
-}
